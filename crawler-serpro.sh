775f5ab4c88ad250b1424ffa76d178675e0ca78e
Fernando Ghinzelli <fghinzelli@gmail.com>
2023-09-12 12:17:52 -0300

    Correção de url

--- crawler-serpro.sh
+++ crawler-serpro.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 PRC_PATH=''
-export SERPRO_URL_BASE='https://www.procergs.rs.gov.br/concurso-publico-2023'
+export SERPRO_URL_BASE='https://www.cebraspe.org.br/concursos/serpro_23'
 export SERPRO_URL_JSON='https://api.cebraspe.org.br/eventos/serpro_23'
 export SERPRO_FILE_NAME=${PRC_PATH}'/crawler-serpro.txt'
 export SERPRO_EMAIL_ORIGIN=''
